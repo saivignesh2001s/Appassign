db72473be193e1a0fe7f87ec3c500bf3d85b53d0
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-18 17:08:52 +0530

    modified

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace UserappAugAssign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname, lname;
+            Console.WriteLine("Enter fname");
+            fname = Console.ReadLine();
+            Console.WriteLine("Enter lname");
+            lname = Console.ReadLine();
+            string uname, password;
+            Console.WriteLine("Enter uname");
+            uname = Console.ReadLine();
+            Console.WriteLine("Password");
+            password = Console.ReadLine();
+            if ((fname == uname) && (lname == password)&&(!String.IsNullOrEmpty(uname))&&(!String.IsNullOrEmpty(password)))
+                Console.WriteLine("Successfully logged in");
+            else
+            {
+
+                if (fname != uname && lname != password)
+                    Console.WriteLine("Password and uname unmatched");
+                else if (fname != uname)
+                    Console.WriteLine("uname unmatched");
+                else if ((String.IsNullOrEmpty(uname)) || (String.IsNullOrEmpty(password)))
+                    Console.WriteLine("uname or password can't be null");
+                else
+                    Console.WriteLine("Password unmatched");
+
+     }
+            Console.ReadLine();
+        }
+    }
+}
